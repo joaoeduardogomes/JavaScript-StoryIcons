b20089fcbf905ef17a7f337c4fa2bb0ecd3965e2
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2023-03-22 21:23:27 -0300

    Images list created

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
  Simple game that generates random icons to be used as an inspiration for the user to create a small story.
 
 This project was based on the Story Cubes game.
+
+Images: [max.icons](https://www.flaticon.com/free-icons/titan)
--- src/scripts/icons.js
+++ src/scripts/icons.js
@@ -1,4 +1,310 @@
 const iconsList = [
-    'https://cdn-icons-png.flaticon.com/512/2822/2822374.png',
-    
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/2822/2822374.png',
+        alt: 'Swordsman'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/2822/2822371.png',
+        alt: 'Adventurer'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/2822/2822377.png',
+        alt: 'Wizard'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/2822/2822366.png',
+        alt: 'Barbarian'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/2822/2822370.png',
+        alt: 'Druid'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/2822/2822378.png',
+        alt: 'Magician'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/2822/2822375.png',
+        alt: 'Knight'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/2822/2822363.png',
+        alt: 'Archer'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/2822/2822379.png',
+        alt: 'Assassin'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/3426/3426252.png',
+        alt: 'Fire'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/3839/3839838.png',
+        alt: 'Book'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/3426/3426193.png',
+        alt: 'Earth'
+    },
+    {
+        alt: 'https://cdn-icons-png.flaticon.com/512/3426/3426152.png',
+        alt: 'Ice'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/3943/3943791.png',
+        alt: 'Dragon'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/2822/2822364.png',
+        alt: 'Priest'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/3426/3426204.png',
+        alt: 'Light'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/3426/3426274.png',
+        alt: 'Warlock'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/3426/3426101.png',
+        alt: 'Potion'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/3426/3426271.png',
+        alt: 'Shield'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/2822/2822361.png',
+        alt: 'Crossbow'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/2822/2822359.png',
+        alt: 'Monk'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/3426/3426041.png',
+        alt: 'Skeleton'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/2642/2642119.png',
+        alt: 'Mermaid'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/3426/3426257.png',
+        alt: 'Bandit'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/3426/3426285.png',
+        alt: 'Poison'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/2822/2822368.png',
+        alt: 'Samurai'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/2822/2822376.png',
+        alt: 'Ninja'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/4793/4793204.png',
+        alt: 'Hidra'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/4793/4793197.png',
+        alt: 'Cerberus'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/2642/2642172.png',
+        alt: 'Excalibur'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/4793/4793215.png',
+        alt: 'Pegasus'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/4793/4793207.png',
+        alt: 'Chimera'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/2822/2822380.png',
+        alt: 'Thief'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/3426/3426155.png',
+        alt: 'Lightning'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/3426/3426181.png',
+        alt: 'Tornado'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/3426/3426149.png',
+        alt: 'Freeze'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/2136/2136635.png',
+        alt: 'Werewolf'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/3426/3426264.png',
+        alt: 'Card'
+    }
+
+]
+
+const treasureHunter = [
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006900.png',
+        alt: 'Crown'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006915.png',
+        alt: 'Hieroglyph'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006932.png',
+        alt: 'Mayan'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7007/7007089.png',
+        alt: 'Crystals'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7007/7007020.png',
+        alt: 'Map'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006898.png',
+        alt: 'Binoculars'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006950.png',
+        alt: 'Skeleton with crown'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006944.png',
+        alt: 'Skeleton'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006912.png',
+        alt: 'Mummy'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006939.png',
+        alt: 'Terracotta army'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006922.png',
+        alt: 'Pyramid'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006971.png',
+        alt: 'Treasure chest'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006927.png',
+        alt: 'Maya pyramid'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7007/7007069.png',
+        alt: 'Dinosaur skull'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006896.png',
+        alt: 'Explorer with map'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006891.png',
+        alt: 'Vase'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7007/7007064.png',
+        alt: 'Treasure'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7007/7007037.png',
+        alt: 'Torch'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7007/7007029.png',
+        alt: 'Compass'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006893.png',
+        alt: 'Coins'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006904.png',
+        alt: 'Explorer with crystal'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7007/7007053.png',
+        alt: 'Gun'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7007/7007014.png',
+        alt: 'Monster chest'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7007/7007085.png',
+        alt: 'Gems'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7007/7007058.png',
+        alt: 'Skull'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006897.png',
+        alt: 'Explorer with torch'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006890.png',
+        alt: 'Key'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7007/7007075.png',
+        alt: 'Gold'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006979.png',
+        alt: 'Empty chest'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7007/7007024.png',
+        alt: 'Revolver'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7007/7007000.png',
+        alt: 'Closed chest'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006894.png',
+        alt: 'Lamp'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7007/7007009.png',
+        alt: 'Cup'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7007/7007043.png',
+        alt: 'Hat & whip'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006899.png',
+        alt: 'Dig'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006895.png',
+        alt: 'Trap'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7006/7006961.png',
+        alt: 'Buried treasure'
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/7007/7007048.png',
+        alt: 'Tools'
+    },
 ]
