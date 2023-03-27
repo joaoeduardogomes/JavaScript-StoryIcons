9b1cbbb75aa9f6f3f012e762a145e1784def1581
João Eduardo Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2023-03-27 16:32:16 -0300

    JS: created sortIcons function
    Now we can generate random icons and add them into an array.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,27 @@
 import { AdventureIcons, treasureHunterIcons } from "./icons.js";
 
-console.log(treasureHunterIcons);
+// Sort icons function:
+function sortIcons(iconsList) {
+    const sortedIcons = [];
+
+    while (sortedIcons.length < 9) {
+        const sortedNumber = Math.floor(Math.random() * iconsList.length); //exclui o último valor.
+
+        const selectedIcon = iconsList[sortedNumber];
+
+        if (!sortedIcons.includes(selectedIcon)) {
+            sortedIcons.push(selectedIcon);
+        }
+    }
+
+    return sortedIcons;
+}
+
+
+
+// Add event listener to btn:
+const button = document.getElementById('btn');
+button.addEventListener('click', () => {
+    const result = sortIcons(treasureHunterIcons);
+    console.log(result);
+});
