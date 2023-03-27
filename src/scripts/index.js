a9fb34a4d02cfee561021505e371fe2fe3347f08
João Eduardo Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2023-03-27 16:53:05 -0300

    JS: created the 'createCubes' function'
    Now we create the remaining 8 cubes for our layout.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -17,11 +17,36 @@ function sortIcons(iconsList) {
     return sortedIcons;
 }
 
+function createCubes() {
+    const iconsSection = document.querySelector('section#icons');
 
+    for(let i = 1; i < 9; i++) {
+        // The external div:
+        const cube = document.createElement('div');
+        cube.classList.add('cube');
+        cube.id = `cube-${i+1}`;
+
+        // The abbr to the img:
+        const abbr = document.createElement('abbr');
+
+        // The img:
+        const img = document.createElement('img');
+
+        // Appending elements (from the inner element):
+        abbr.appendChild(img);
+        cube.appendChild(abbr);
+        iconsSection.appendChild(cube);
+    }
+
+    return iconsSection;
+}
 
 // Add event listener to btn:
 const button = document.getElementById('btn');
 button.addEventListener('click', () => {
     const result = sortIcons(treasureHunterIcons);
     console.log(result);
+
+    const section = createCubes();
+    console.log(section);
 });
